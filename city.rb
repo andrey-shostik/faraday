571b909bc5e6419439f3e08a1ac8ddbab47e9eb0
andrey9828 <aaa73731919@gmail.com>
2016-02-20 22:01:18 +0200

    update and added say_weather method

--- city.rb
+++ city.rb
@@ -3,25 +3,30 @@ require 'json'
 
 class City
   API_KEY = '1ecd61a1ba5714cdc22ea4ea7f63ed23'
+  K = 273.15
   attr_accessor :name
 
-  def initialize(name = 'San-Francisco')
+  def initialize(name = 'Kaniv')
     @name = name
   end
 
+  def say_weather
+    puts "now weather in #{@name}"
+    (conditions['main']['temp']) - K
+  end
+
+  private
   def conditions
     if @conditions.nil?
-      response = connection.get("/api/#{API_KEY}/conditions/q/CA/#{name.gsub('-','_')}.json")
+      response = connection.get("/data/2.5/weather?q=#{name.gsub('-','_')},uk&appid=#{API_KEY}")
       @conditions = JSON.parse(response.body)
-      puts @conditions
     else
       @conditions
     end
   end
 
-  private
   def connection
-    @connection ||= Faraday.new(url: 'http://api.wunderground.com') do |faraday|
+    @connection ||= Faraday.new(url: 'http://openweathermap.org/') do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
     end
@@ -29,4 +34,4 @@ class City
 end
 
 a = City.new
-a.conditions
+puts a.say_weather
